e31606be07de941b80730cf847e269ed62b0e47f
Sebastian Palm <sebastian.palm91@gmail.com>
2017-11-30 12:46:55 +0100

    First push

---
+++ puzzleJS.js
@@ -0,0 +1,34 @@
+// TODO: 1, Make an array of the puzzleCards
+//       2, Make each puzzleCard get a random position
+//       3, Make each PuzzleCard get a random number
+
+const randomNum = Math.floor(Math.random() * 10) + 1;
+const puzzleCard = document.querySelectorAll('.puzzle');
+const puzzleList = Array.from(puzzleCard);
+// const removeCard = document.querySelectorAll('.puzzle');
+// const dataSet = document.querySelectorAll('.puzzle');
+
+
+const puzzleArray = [];
+
+const compare = (dataset, callback) => {
+  if (puzzleArray.length == 2) {
+    if (puzzleArray[0] == puzzleArray[1]) {
+      console.log('AYE');
+      puzzleArray = [];
+    } else {
+      console.log('NO');
+      puzzleArray = [];
+    }
+  }
+}
+
+
+
+Array.from(puzzleCard).forEach( (puzzleCard) => {
+    puzzleCard.addEventListener('click', (e) => {
+      puzzleCard.classList.toggle('turn');
+      const dataset = e.target.dataset.puzzle;
+      console.log(dataset);
+    })
+})
