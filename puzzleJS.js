041fda653f44bc044f6ab71f6847183350ee99fb
Sebastian Palm <sebastian.palm91@gmail.com>
2017-11-30 21:25:45 +0100

    added shuffle function to the cards

--- puzzleJS.js
+++ puzzleJS.js
@@ -14,6 +14,11 @@ for (var i = container.children.length; i >= 0; i--) {
   container.appendChild(container.children[Math.random() * i | 0]);
 }
 
+// resetGame.addEventListener('click', () => {
+//
+// });
+
+//Creating empty Arrays
 let puzzleArray = [];
 let completed = [];
 
@@ -30,8 +35,8 @@ let compare = (dataset, callback) => {
       setTimeout(function(){
         completed[1].classList.remove('turn')
         completed[0].classList.remove('turn')
-        completed[1].classList.remove('turn')
         completed[0].classList.remove('turn')
+        completed[1].classList.remove('turn')
         puzzleArray = [];
         completed = [];
         console.log('NO');
@@ -43,6 +48,7 @@ let compare = (dataset, callback) => {
 //Looping through my array and reset the game
 Array.from(puzzleCard).forEach( (puzzleCard) => {
   resetGame.addEventListener('click', () => {
+    puzzleCard.classList.add('turn');
     puzzleCard.classList.remove('turn');
     puzzleCard.classList.remove('success');
     for (var i = container.children.length; i >= 0; i--) {
@@ -51,6 +57,31 @@ Array.from(puzzleCard).forEach( (puzzleCard) => {
     puzzleArray = [];
     completed = [];
   })
+  resetGame.addEventListener('click', (e) => {
+    puzzleCard.classList.add('transition')
+    for (var i = 0; i < puzzleArray.length -16; i++) {
+      let currentLeft = puzzleArray[i].getBoundingClientRect().left
+      let currentTop = puzzleArray[i].getBoundingClientRect().top
+      let currentRight = puzzleArray[i].getBoundingClientRect().right
+      let currentBottom = puzzleArray[i].getBoundingClientRect().bottom
+
+      let nextLeft = puzzleArray[i + 1].getBoundingClientRect().left
+      let nextTop = puzzleArray[i + 1].getBoundingClientRect().top
+      let nextRight = puzzleArray[i + 1].getBoundingClientRect().right
+      let nextBottom = puzzleArray[i + 1].getBoundingClientRect().bottom
+
+      puzzleArray[i].style.left = nextLeft + 'px';
+      puzzleArray[i].style.top = nextTop + 'px';
+      puzzleArray[i].style.right = nextRight + 'px';
+      puzzleArray[i].style.bottom = nextBottom + 'px';
+
+      puzzleArray[i].style.left = currentLeft + 'px';
+      puzzleArray[i].style.top = currentTop + 'px';
+      puzzleArray[i].style.right = currentRight + 'px';
+      puzzleArray[i].style.bottom = currentBottom + 'px';
+    }
+    console.log(puzzleArray[i]);
+    })
   // Creating a clickevent for the cards and calling the callback function
   puzzleCard.addEventListener('click', (e) => {
     puzzleCard.classList.toggle('turn');
@@ -61,6 +92,8 @@ Array.from(puzzleCard).forEach( (puzzleCard) => {
   })
 })
 
+
+//first try shuffle
 // resetGame.addEventListener('click', () => {
 // for (var i = 0; i < puzzleList.length -1; i++) {
 //   let currentLeft = puzzleList[i].getBoundingClientRect().left
@@ -86,3 +119,21 @@ Array.from(puzzleCard).forEach( (puzzleCard) => {
 // }
 //
 // });
+//second try shuffle
+// Array.prototype.move = function (puzzleList, newArray) {
+//     while (puzzleList < 0) {
+//       puzzleList += this.length;
+//     }
+//     while (newArray < 0) {
+//       newArray += this.length;
+//     }
+//     if (newArray >= this.length) {
+//       var k = newArray - this.length;
+//       while ((k--) + 1) {
+//         this.push(undefined);
+//       }
+//     }
+//     this.splice(newArray, 0, this.splice(puzzleList, 1)[0]);
+//     return this;
+//     console.log(Array.prototype.move);
+//   };
