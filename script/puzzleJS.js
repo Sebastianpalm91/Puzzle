f0662f216842f8b143328164290a07b40a8b61dc
Sebastian Palm <sebastian.palm91@gmail.com>
2017-12-04 18:59:49 +0100

    addded a clocktimer when the user turns first card

--- script/puzzleJS.js
+++ script/puzzleJS.js
@@ -13,6 +13,10 @@ const resetGame = document.querySelector('button');
 const container = document.querySelector('.puzzleContainer');
 const boxShadow = document.querySelectorAll('boxShadow');
 const puzzleList = Array.from(puzzleCard);
+// Clock timer
+var h2 = document.getElementsByTagName('h2')[0],
+    seconds = 0, minutes = 0, hours = 0,
+    t;
 
 //Random numbers given to each elements data-set-'value' befor start
 for (var i = container.children.length; i >= 0; i--) {
@@ -42,24 +46,22 @@ let compare = (dataset, callback) => {
            cardSuccesBox.style.display = "block";
         }, 1000);
       }
-    } else {
+    }
+    else {
       setTimeout(function(){
-          var myVar = setInterval(function(){ myTimer() }, 3000);
-
-          function myTimer() {
-              var d = new Date();
-              var t = d.toLocaleTimeString();
-              document.puzzleCard.innerHTML = t;
-          }
         completed[1].classList.remove('turn')
         completed[1].classList.remove('turn')
         completed[0].classList.remove('turn')
         completed[0].classList.remove('turn')
-        completed[1].classList.remove('turn')
         puzzleArray = [];
         completed = [];
       },1000);
     }
+  }
+  if (puzzleArray.length == 3) {
+      completed[0].classList.remove('turn')
+      completed[1].classList.remove('turn')
+      completed[2].classList.remove('turn')
   }
 };
 //Looping through my array
@@ -68,6 +70,22 @@ Array.from(puzzleCard).forEach( (puzzleCard) => {
   // Creating a clickevent for the cards and calling the callback function
   puzzleCard.addEventListener('click', (e) => {
     puzzleCard.classList.toggle('turn');
+    function add() {
+        seconds++;
+        if (seconds >= 60) {
+            seconds = 0;
+            minutes++;
+        }
+        h2.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+        timer();
+    }
+    function timer() {
+        t = setTimeout(add, 1000);
+    }
+    timer();
+
+    // starttimer = "true";
+	// 	timer();
     const dataset = e.target.dataset.puzzle;
     const puzzleTarget = puzzleArray.push(dataset);
     completed.push(puzzleCard);
@@ -77,6 +95,9 @@ Array.from(puzzleCard).forEach( (puzzleCard) => {
   })
   //Resetting the game and shuffle the cards
   resetGame.addEventListener('click', () => {
+    h2.textContent = "00:00:00";
+    seconds = 0; minutes = 0; hours = 0;
+    clearTimeout(t);
     puzzleCard.classList.remove('turn');
     puzzleCard.classList.remove('success');
     puzzleCard.classList.remove('boxShadow');
@@ -111,3 +132,34 @@ Array.from(puzzleCard).forEach( (puzzleCard) => {
     }, 1800);
   })
 })
+
+
+
+
+
+
+
+
+
+// var i = 0;
+// var starttimer= "false";
+//
+//
+// function timer(){
+// 		if(starttimer=="true"){
+// 			setTimeout(function(){
+// 				i=i+0.01;
+// 				$(".timer").html("<h1><center>"+ i.toFixed(2) + "</center></h1>");
+// 				timer();
+// 			},10);
+// 		};
+// };
+// resetGame.addEventListener('click', resetGameCallback);
+
+// var myVar = setInterval(function(){ myTimer() }, 3000);
+//
+// function myTimer() {
+//     var d = new Date();
+//     var t = d.toLocaleTimeString();
+//     document.puzzleCard.innerHTML = t;
+// }
