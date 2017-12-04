a6ff59b1d59eddb4c7550c211abff1cb2d50a6a6
Sebastian Palm <sebastian.palm91@gmail.com>
2017-12-04 16:51:30 +0100

    added more TODO and trying to fix the bug when turning 3 cards fast

--- script/puzzleJS.js
+++ script/puzzleJS.js
@@ -3,6 +3,8 @@
 //       3, Make each PuzzleCard get a random number
 //       4, Make a resetButton and give it a random number
 //       5, Make a visually displayed shuffle function
+//       6, Make the user able to choose difficulty
+//       7, Fix the BUG when turning 3 cards fast the 1st wont turn back
 
 //Declaring my variables
 const puzzleCard = document.querySelectorAll('.puzzle');
@@ -11,10 +13,12 @@ const resetGame = document.querySelector('button');
 const container = document.querySelector('.puzzleContainer');
 const boxShadow = document.querySelectorAll('boxShadow');
 const puzzleList = Array.from(puzzleCard);
+
 //Random numbers given to each elements data-set-'value' befor start
 for (var i = container.children.length; i >= 0; i--) {
   container.appendChild(container.children[Math.random() * i | 0]);
 }
+
 
 
 //Creating empty Arrays and a scorecounter
@@ -32,6 +36,7 @@ let compare = (dataset, callback) => {
       completed = [];
       cardSucces++;
       console.log(cardSucces);
+      //IF all cards completed, display the winning box
       if (cardSucces === 8) {
         setTimeout(function() {
            cardSuccesBox.style.display = "block";
@@ -39,6 +44,14 @@ let compare = (dataset, callback) => {
       }
     } else {
       setTimeout(function(){
+          var myVar = setInterval(function(){ myTimer() }, 3000);
+
+          function myTimer() {
+              var d = new Date();
+              var t = d.toLocaleTimeString();
+              document.puzzleCard.innerHTML = t;
+          }
+        completed[1].classList.remove('turn')
         completed[1].classList.remove('turn')
         completed[0].classList.remove('turn')
         completed[0].classList.remove('turn')
@@ -51,6 +64,7 @@ let compare = (dataset, callback) => {
 };
 //Looping through my array
 Array.from(puzzleCard).forEach( (puzzleCard) => {
+    puzzleCard.classList.add('boxShadow');
   // Creating a clickevent for the cards and calling the callback function
   puzzleCard.addEventListener('click', (e) => {
     puzzleCard.classList.toggle('turn');
@@ -58,10 +72,10 @@ Array.from(puzzleCard).forEach( (puzzleCard) => {
     const puzzleTarget = puzzleArray.push(dataset);
     completed.push(puzzleCard);
     puzzleCard.classList.add('boxShadow');
+    console.log(e.target.dataset.puzzle);
     return compare(e.target.dataset.puzzle);
   })
   //Resetting the game and shuffle the cards
-
   resetGame.addEventListener('click', () => {
     puzzleCard.classList.remove('turn');
     puzzleCard.classList.remove('success');
@@ -97,76 +111,3 @@ Array.from(puzzleCard).forEach( (puzzleCard) => {
     }, 1800);
   })
 })
-
-
-
-
-
-
-
-
-
-
-// resetGame.addEventListener('click', () => {
-//   // puzzleCard.classList.add('transition');
-//   for (var i = 0; i < puzzleList.length; i++) {
-//     Array.prototype.move = function(puzzleList, puzzleArray) {
-//       var x = this[puzzleList];
-//
-//       this.splice(puzzleList, 1);
-//       this.splice(puzzleArray,0,x);
-//     }
-//     const arr = [puzzleList];
-//     puzzleCard.element = arr;
-//     arr.move(2,1);
-//     puzzleCard.element += ' ' + arr;
-//   }
-//   console.log(puzzleList);
-// })
-
-// Trying to add a shuffle function to the cards
-
-//first try shuffle
-// resetGame.addEventListener('click', () => {
-// for (var i = 0; i < puzzleArray.length -16; i++) {
-//   let currentLeft = puzzleArray[i].getBoundingClientRect().left
-//   let currentTop = puzzleArray[i].getBoundingClientRect().top
-//   let currentRight = puzzleArray[i].getBoundingClientRect().right
-//   let currentBottom = puzzleArray[i].getBoundingClientRect().bottom
-//
-//   let nextLeft = puzzleArray[i + 1].getBoundingClientRect().left
-//   let nextTop = puzzleArray[i + 1].getBoundingClientRect().top
-//   let nextRight = puzzleArray[i + 1].getBoundingClientRect().right
-//   let nextBottom = puzzleArray[i + 1].getBoundingClientRect().bottom
-//
-//   puzzleArray[i].style.left = nextLeft + 'px';
-//   puzzleArray[i].style.top = nextTop + 'px';
-//   puzzleArray[i].style.right = nextRight + 'px';
-//   puzzleArray[i].style.bottom = nextBottom + 'px';
-//
-//   puzzleArray[i].style.left = currentLeft + 'px';
-//   puzzleArray[i].style.top = currentTop + 'px';
-//   puzzleArray[i].style.right = currentRight + 'px';
-//   puzzleArray[i].style.bottom = currentBottom + 'px';
-// }
-// console.log(puzzleArray[i]);
-//
-// });
-//second try shuffle
-// Array.prototype.move = function (puzzleList, newArray) {
-//     while (puzzleList < 0) {
-//       puzzleList += this.length;
-//     }
-//     while (newArray < 0) {
-//       newArray += this.length;
-//     }
-//     if (newArray >= this.length) {
-//       var k = newArray - this.length;
-//       while ((k--) + 1) {
-//         this.push(undefined);
-//       }
-//     }
-//     this.splice(newArray, 0, this.splice(puzzleList, 1)[0]);
-//     return this;
-//     console.log(Array.prototype.move);
-//   };
